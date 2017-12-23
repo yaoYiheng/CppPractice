5d98f71172f5ef5012cfd206b76fa04e11dfb997
YihengYao <fn1117@qq.com>
2017-12-23 22:21:58 +1300

    17_12_23_模版函数
    所谓函数模板，实际上是建立一个通用函数，它所用到的数据的类型（包括返回值类型、形参类型、局部变量类型）可以不具体指定，而是用一个虚拟的类型来代替（实际上是用一个标识符来占位），等发生函数调用时再根据传入的实参来逆推出真正的类型。这个通用函数就称为函数模板（Function Template）。

--- 17_12_23_函数模版基本语法/17_12_23_函数模版基本语法/main.cpp
+++ 17_12_23_函数模版基本语法/17_12_23_函数模版基本语法/main.cpp
@@ -7,16 +7,28 @@
 //
 
 #include <iostream>
-
-template <class T>
+/*
+     在C++中，数据的类型也可以通过参数来传递，在函数定义时可以不指明具体的数据类型，当发生函数调用时，编译器可以根据传入的实参自动推断数据类型。这就是类型的参数化。
+一但定义了函数模板，就可以将类型参数用于函数定义和函数声明了。说得直白一点，原来使用 int、float、char 等内置类型的地方，都可以用类型参数来代替
+ */
+template <class T>//仅对这之下的函数有效. 或template <Type T> 告诉编译器下面写的是模版函数
 void Swap(T &a, T &b) {
     T temp = a;
     a = b;
     b = temp;
 }
 
+void test1(){
+
+    int a = 10, b = 20;
+    std::cout << "a: " << a << "b: " << b << std::endl;
+    Swap(a, b);
+    std::cout << "a: " << a << "b: " << b << std::endl;
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    //调用
+    test1();
     return 0;
 }
